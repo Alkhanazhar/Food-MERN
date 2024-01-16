8c13a79c40d4e45de6d6f45dd6317d254a9206c2
Azhar Khan <107482530+Alkhanazhar@users.noreply.github.com>
2024-01-16 22:46:04 +0530

    Delete backend directory

--- backend/controllers/foodRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const Food = require("../models/Food")
-const router = express.Router()
-router.get("/foods", async (req, res) => {
-    const foods = await Food.find({})
-    res.json(foods)
-})
-module.exports = router
--- backend/models/Food.js
+++
@@ -1,9 +0,0 @@
-const {Schema,model}=require("mongoose")
-const foodSchema=new Schema({
-    name:String,
-    price:String,
-    desc:String,
-    img:String
-})
-const Food=model("Food",foodSchema)
-module.exports=Food
