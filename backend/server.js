bcc981c7c73d746da87f2bd460a91a6e0016370f
Azhar Khan <107482530+Alkhanazhar@users.noreply.github.com>
2024-01-16 22:32:17 +0530

    env

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
-const { default: mongoose } = require("mongoose")
-const app=require("./app")
+const { default: mongoose, mongo } = require("mongoose")
+const app = require("./app")
 const Database = require("./database")
-const port=8000
+const port = process.env.PORT || 8000
 try {
-     Database.connect()
+    Database.connect()
     console.log("db connected")
-    app.listen(8000,()=>{console.log("server connected 8000")})
+    app.listen(port, () => { console.log("server connected 8000") })
 } catch (e) {
     console.log("error")
 }
+git push origin master
